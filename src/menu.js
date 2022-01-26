95801bed31eb033d4c3254c6c03fc638f5521b6b
jordi <jordi.lanau@yahoo.com>
2022-01-26 16:16:26 +0000

    Break down long query strings

--- src/menu.js
+++ src/menu.js
@@ -67,13 +67,19 @@ function viewAllDepartments() {
 
 function viewAllRoles() {
   const query =
-    'SELECT ROLES.id, ROLES.title, DEPARTMENTS.name AS department, ROLES.salary FROM ROLES JOIN DEPARTMENTS ON department_id = DEPARTMENTS.id ORDER BY id';
+    'SELECT ROLES.id, ROLES.title, DEPARTMENTS.name AS department,' +
+    ' ROLES.salary FROM ROLES JOIN DEPARTMENTS ON department_id =' +
+    ' DEPARTMENTS.id ORDER BY id';
   selectQuery(query);
 }
 
 function viewAllEmployees() {
   const query =
-    "SELECT e1.id, e1.first_name, e1.last_name, DEPARTMENTS.name AS department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON DEPARTMENTS.id = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id = e1.role_id LEFT JOIN EMPLOYEES e2 ON e1.manager_id = e2.id ORDER BY id";
+    'SELECT e1.id, e1.first_name, e1.last_name, DEPARTMENTS.name AS' +
+    " department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' '," +
+    ' e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON DEPARTMENTS.id' +
+    ' = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id = e1.role_id LEFT' +
+    ' JOIN EMPLOYEES e2 ON e1.manager_id = e2.id ORDER BY id';
   selectQuery(query);
 }
 
@@ -83,7 +89,11 @@ async function viewEmployeesByDepartment() {
   );
   const query =
     'SELECT e1.id, e1.first_name, e1.last_name, DEPARTMENTS.name AS' +
-    " department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON DEPARTMENTS.id = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id = e1.role_id LEFT JOIN EMPLOYEES e2 ON e1.manager_id = e2.id WHERE DEPARTMENTS.name = ? ORDER BY id";
+    " department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' '," +
+    ' e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON DEPARTMENTS.id' +
+    ' = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id = e1.role_id LEFT' +
+    ' JOIN EMPLOYEES e2 ON e1.manager_id = e2.id WHERE DEPARTMENTS.name = ?' +
+    ' ORDER BY id';
   selectQuery(query, departmentName);
 }
 
@@ -98,7 +108,11 @@ async function viewEmployeesByManager() {
       const managerID = results[0].id;
       const query =
         'SELECT e1.id, e1.first_name, e1.last_name, DEPARTMENTS.name AS' +
-        " department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON DEPARTMENTS.id = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id = e1.role_id LEFT JOIN EMPLOYEES e2 ON e1.manager_id = e2.id WHERE e2.id = ? ORDER BY id";
+        " department, ROLES.title, ROLES.salary, CONCAT(e2.first_name, ' '," +
+        ' e2.last_name) AS manager FROM DEPARTMENTS JOIN ROLES ON' +
+        ' DEPARTMENTS.id = ROLES.department_id JOIN EMPLOYEES e1 ON ROLES.id' +
+        ' = e1.role_id LEFT JOIN EMPLOYEES e2 ON e1.manager_id = e2.id WHERE' +
+        ' e2.id = ? ORDER BY id';
       selectQuery(query, managerID);
     }
   });
